65d470fa81afbe8e984a2e6a538a2b43fd767ebc
Infinity <573679956@qq.com>
2015-12-01 12:16:11 +0800

    Dec 1, 2015

--- Union (countable).h
+++ Union (countable).h
@@ -3,7 +3,7 @@ struct Union
 public:
     Union(int n) { for (int i = 0; i <= n; i++) anc.push_back(i), cnt.push_back(1); }
     int find(int x) { /* be careful of stack overflow */ if (anc[x] == x) return x; anc[x] = find(anc[x]); return anc[x]; }
-    void connect(int x, int y) { int t = count(x); anc[find(x)] = find(y); cnt[find(y)] += t; }
+    void connect(int x, int y) { if (isConnected(x, y)) return; int t = count(x); anc[find(x)] = find(y); cnt[find(y)] += t; }
     bool isConnected(int x, int y) { return find(x) == find(y); }
     int count(int x) { return cnt[find(x)]; }
 protected:
