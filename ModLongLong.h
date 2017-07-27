7ac41f2610c128abb36e305dafe1da5c26ec21bf
Infinity <573679956@qq.com>
2017-07-27 15:14:39 +0800

    Update ModLongLong.h

--- ModLongLong.h
+++ ModLongLong.h
@@ -7,7 +7,7 @@ class ModLongLong // operator %(int, int) is faster
     { if (b) { exGcd(b, a % b, y, x);  y -= a / b * x; } else { x = 1; y = 0; } }
 
 public:
-    ModLongLong(const ll &a) : data((a % n + n) % n) {}
+    ModLongLong(ll a) : data((a % n + n) % n) {}
     ll toLongLong() const { return data; }
     mll operator +(const mll &b) const { return this->data + b.data; }
     mll operator -(const mll &b) const { return this->data - b.data; }
@@ -15,7 +15,7 @@ public:
     mll operator /(const mll &b) const
     { ll x = 0, y = 0; exGcd(b.data, n, x, y); x = (x + n) % n; return this->data * x; }
     mll pow(ll b) const
-    { mll p = 1; for (mll a = *this; b; a *= a, b >>= 1) if (b & 1ll) p *= a; return p; }
+    { mll p = 1; for (mll a = *this; b; a *= a, b >>= 1) if (b & 1) p *= a; return p; }
     mll operator +=(const mll &b) { return *this = *this + b; }
     mll operator -=(const mll &b) { return *this = *this - b; }
     mll operator *=(const mll &b) { return *this = *this * b; }
