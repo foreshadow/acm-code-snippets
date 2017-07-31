b80a47c2ee0e2c72f69d479caffb09ddd7116849
Infinity <573679956@qq.com>
2017-08-01 00:42:06 +0800

    Compact code, remove unused function

--- Manacher.h
+++ Manacher.h
@@ -1,48 +1,31 @@
 class Manacher
 {
 public:
-    Manacher(const std::string &s) : 
-        d(expand(s)), r(d.size())
+    Manacher(const string &s) : d(expand(s)), r(d.size())
     {
-        int p = 0;
-        int mx = 0;
-        for (int i = 1; i < d.length() - 1; i++)
-        {
-            if (mx > i)
-                r[i] = min(r[2 * p - i], mx - i);
-            else
-                r[i] = 1;
-            while (d[i + r[i]] == d[i - r[i]])
-                r[i]++;
-            if (i + r[i] > mx)
-            {
-                p = i;
-                mx = i + r[i];
-            }
+        int p = 0, mx = 0;
+        for (int i = 1; i < int(d.length()) - 1; i++) {
+            r[i] = mx > i ? min(r[2 * p - i], mx - i) : 1;
+            while (d[i + r[i]] == d[i - r[i]]) { r[i]++; }
+            if (i + r[i] > mx) { p = i; mx = i + r[i]; }
         }
     }
-
-    std::string &data()
-    { return d; }
 
     int &operator [](unsigned n)
     { return r[n]; }
 
 private:
-    static std::string expand(const std::string &str)
+    static string expand(const string &str)
     {
-        std::string s("$#");
-        s.reserve(str.size() * 2 + 1);
-        for (char c : str)
-        {
-            s.push_back(c);
-            s.push_back('#');
+        string s("$#");
+        for (char c : str) {
+            s.push_back(c); s.push_back('#');
         }
         s.push_back('@');
         return s;
     }
 
 protected:
-    std::string d;
-    std::vector<int> r;
+    string d;
+    vector<int> r;
 };
